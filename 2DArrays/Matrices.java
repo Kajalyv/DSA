639d3f9cd7e3fd81c8eff1df951a10be1b88f732
Kajalyv <154503139+Kajalyv@users.noreply.github.com>
2026-06-05 23:43:53 +0530

    Fix output loop to increment row index correctly

--- 2DArrays/Matrices.java
+++ 2DArrays/Matrices.java
@@ -30,7 +30,7 @@ public class Matrices {
         //output
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
         }
