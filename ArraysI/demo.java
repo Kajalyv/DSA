625dae59f5ca3f86e70c781960c0c9564bf72b5c
Kajalyv <154503139+Kajalyv@users.noreply.github.com>
2026-06-07 22:24:24 +0530

    Update demo.java

--- ArraysI/demo.java
+++ ArraysI/demo.java
@@ -7,7 +7,7 @@ public class demo {
         }
     }
     public static void main(String agrs[]){
-        int marks[] = {97, 99, 98};
+        int marks[] = {97, 99, 98, 100};
         update(marks);
         for(int i=0; i<marks.length; i++){
             System.out.print(marks[i]+" ");
@@ -15,3 +15,4 @@ public class demo {
         System.out.println();
     }
 }
+
