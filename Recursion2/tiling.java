ecc500a20faa5f8f38b543fc908ce4ac514ba7cd
kajalyv <kajalyv6743@gmail.com.in>
2026-06-12 16:51:09 +0530

    tiling problem

---
+++ Recursion2/tiling.java
@@ -0,0 +1,18 @@
+public class tiling {
+    public static int tilingProblem(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        //vertical choice
+        int fnm1 = tilingProblem(n-1);
+
+        //horizontal choice 
+        int fnm2 = tilingProblem(n-2);
+
+        int toWays = fnm1 + fnm2;
+        return toWays;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(3));
+    }
+}
